415a70165b5f329e71795fc4796f8ce2d735cdf7
Jessy LEBRUN <Jessy.Lebrun.Etu@univ-lemans.fr>
2022-05-21 11:38:19 +0200

    V0.0.4 Random choice of themes and categories

--- model/Question.java
+++ model/Question.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Question {
     private String theme;
     private String category;
@@ -7,10 +10,19 @@ public class Question {
     private String question;
     private String[] propositions;
     private String answer;
+    private String level;
 
-    public Question() {
+    public Question() throws Exception {
         //TODO Extract data for a random question and
         // put the corresponding data in the class fields
+        ArrayList<String> list = lsDir(treeQuestion(null));
+        theme = list.get((int)(Math.random() * list.size()));
+
+        list = lsDir(treeQuestion(new String[]{theme}));
+        category = list.get((int)(Math.random() * list.size()));
+
+        File question = treeQuestion(new String[] {theme, category});
+
     }
 
     public String getTheme() {
@@ -59,5 +71,45 @@ public class Question {
 
     public void setAnswer(String answer) {
         this.answer = answer;
+    }
+
+    /**
+     * lsDirQuestion
+     * @param pathDir ([]|null : theme, [theme] : cathegorie, [theme, cathegorie] : question.json
+     * @return
+     */
+    private static File treeQuestion(String[] pathDir) throws Exception {
+        if (pathDir == null) pathDir = new String[]{};
+        if (pathDir.length > 2) throw new Exception("Table too big");
+        String path = "questionJSON";
+        if (System.getProperty("os.name").split(" ")[0].equals("Windows")){
+            for (int i = 0; i < pathDir.length; ++i){
+                if (i < pathDir.length) path += "\\";
+                path += pathDir[i];
+
+            }
+            if (pathDir.length == 2) path += "\\question.json";
+        } else { //Linux
+            for (int i = 0; i < pathDir.length; ++i){
+                if (i < pathDir.length) path += "/";
+                path += pathDir[i];
+            }
+            if (pathDir.length == 2) path += "/question.json";
+        }
+        return new File(path);
+    }
+
+    private static ArrayList<String> lsDir (File disParent){
+        ArrayList<String> listDir = new ArrayList();
+        for (File dirChild: disParent.listFiles()){
+            if(dirChild.isDirectory()) listDir.add(dirChild.getName());
+        }
+        return listDir;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Question q = new Question();
+        System.out.println("theme : "+ q.theme);
+        System.out.println("category : "+ q.category);
     }
 }
